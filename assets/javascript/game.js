25cab2e9b76a4153bc7abdafd58b62c6f68e23c8
GregH <43968370+gfhorrocks@users.noreply.github.com>
2019-02-23 01:26:18 -0500

    GAME LOGIC DONE, NOW FOR CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+//MAIN ARRAY DECLARATION FOR THE MATRIX GAME
+
 var word = ["wachowskis",
     "revolutions",
     "reloaded",
@@ -24,53 +26,77 @@ var word = ["wachowskis",
     "jackson",
     "thompson"];
 
-var wins = 0;
-var guessesLeft = 10;
+//VARIABLE DECLARATIONS
 
+var wins = 0;                
+var guessesLeft = 10;
+var incorrectFlag = 0;
 var alertText = document.getElementById("alert-text");
 var winsText = document.getElementById("wins-text");
 var currentText = document.getElementById("current-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var guessesText = document.getElementById("guesses-text");
-
-
 var index = Math.floor(Math.random() * 26);
 var tempWord = [word[index].length];
 var correctLetters = 0;
+
+//FILLING THE TEMPWORD WITH UNDERSCORES
 
 for (var x = 0; x < word[index].length; x++) {
     tempWord[x] = "_";
 }
 
-document.onkeyup = function (event) {
+//ON KEY UP FUNCTION
 
+document.onkeyup = function (event) {
+    console.clear();
     var userGuess = event.key;
     var fullWord = "";
 
+    incorrectFlag = 1;
     alertText.textContent = "";
+    guessesLeftText.textContent = guessesLeft;
 
-    for (var i = 0; i < word[index].length; i++) {
-        if (tempWord[i] === "_") {
+//CHECKING TO SEE IF LETTER HAS BEEN USED FIRST
+
+    if (usedBefore(userGuess, guessesText.textContent) === false) { 
+        for (var i = 0; i < word[index].length; i++) {
+
             if (userGuess === word[index].charAt(i)) {
+
                 tempWord[i] = word[index].charAt(i);
                 correctLetters++;
-                incorrect=0;
+                incorrectFlag = 0;
             }
+
+            fullWord += " " + tempWord[i] + " ";
         }
 
-        fullWord += " " + tempWord[i] + " ";
+        currentText.textContent = fullWord;
+        guessesText.textContent += userGuess;
+
+    }
+    else {
+        incorrectFlag = 0;
     }
 
-    currentText.textContent = fullWord;
-    guessesLeftText.textContent = guessesLeft;
-    guessesText.textContent += userGuess;
+//IF IT MAKES IT HERE WITH FLAG 1 - IT NEVER FOUND THE LETTER IN THE STRING
+
+    if (incorrectFlag === 1) {
+
+        guessesLeft--;
+        guessesLeftText.textContent = guessesLeft;
+
+
+    }
+
+//WIN CONDTION WHEN THE NUMBER OF CORRECT NUMBERS IS = TO THE LENGTH OF STRING
 
     if (correctLetters === word[index].length) {
         wins++;
         guessesLeft = 10;
         guessedLetters = "";
         correctLetters = 0;
-        attempt = 0;
         index = Math.floor(Math.random() * 26);
         tempWord = [word[index].length];
 
@@ -85,6 +111,48 @@ document.onkeyup = function (event) {
         guessesText.textContent = "";
     }
 
-    console.log("Word: " + word[index]);
-    console.log("Count: " + correctLetters);
+//LOSE CONDITION WHEN THE NUMBER OF GUESSESLEFT IS 0
+
+    if (guessesLeft === 0) {
+        guessesLeft = 10;
+        guessedLetters = "";
+        correctLetters = 0;
+        index = Math.floor(Math.random() * 25);
+        tempWord = [word[index].length];
+
+        for (var x = 0; x < word[index].length; x++) {
+            tempWord[x] = "_";
+        }
+
+        alertText.textContent = "I'm sorry but you failed to get the word right! Press a Key to start a new game!";
+        winsText.textContent = wins;
+        currentText.textContent = "";
+        guessesLeftText.textContent = "";
+        guessesText.textContent = "";
+    }
+
+//RESERVED FOR CONSOLE LOGGING (USED FOR HOURS AND HOURS)
+
+    // console.log("Word: " + word[index]);
+    // console.log("Letter Guessed: " +userGuess);
+    // console.log("Correct Letters" + correctLetters);
+    // console.log("Guess Text: " +guessesText.textContent);
+    // console.log("Guesses Left: " +guessesLeft);
+    // console.log("Incorrect Flag: " +incorrectFlag);
+    // console.log("TempWord: " +tempWord);
+    // console.log("Guesses Text: " +guessesLeftText.textContent);
+}
+
+//SIMPLE USEDBEFORE FUNCTION TO RETURN TRUE OR FALSE
+
+function usedBefore(gLetter, guessesT) {
+
+    for (i = 0; i < guessesT.length; i++) {
+
+        if (gLetter === guessesT.charAt(i)) {
+
+            return true;
+        }
+    }
+    return false;
 }
